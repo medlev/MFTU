14aa528e04f2db6c530fb5fd850ea3e954bd668a
medlev <norkoff@gmail.com>
2018-07-02 18:48:39 +0300

    коллекции пройдены

--- week2/set.py
+++ week2/set.py
@@ -20,6 +20,7 @@ print(even_set)
 """
 
 #example 3
+"""
 odd_set = set()
 even_set = set()
 for number in range(10):
@@ -42,7 +43,18 @@ print(difference_set)
 symmetric_difference_set = odd_set ^ even_set
 symmetric_difference_set = odd_set.symmetric_difference(even_set)
 print(symmetric_difference_set)
-
+"""
 #example 4
-even_set.remove(2)
-print(even_set)
+import random
+random_set = set()
+while True:
+    new_number = random.randint(1, 10)
+    if new_number in random_set:
+        break
+    random_set.add(new_number)
+print(len(random_set) + 1)
+
+empty_list = []
+empty_list = list()
+none_list = ["123", 1123]
+print(none_list[-1])
